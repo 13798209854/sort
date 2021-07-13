3476c947d111b52b3e19d0adb1360645ea627ed4
Simon <48599738+13798209854@users.noreply.github.com>
2021-07-13 16:33:26 +0800

    1.增加插入排序代码

---
+++ InsertSort/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,42 @@
+﻿#include <vector>
+#include <iostream>
+
+
+void insert_sort(int a[], int size)
+{
+	if (size < 2) return;
+	for (int i = 1; i < size; ++i)
+	{
+		for (int j = i; j >= 1; --j)
+		{
+			if (a[j] < a[j - 1])
+			{
+				auto t = a[j];
+				a[j] = a[j - 1];
+				a[j - 1] = t;
+			}
+		}
+	}
+}
+
+template<typename T>
+void printArray(T a[], int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		std::cout << *(a+i)<<" ";
+	}
+	std::cout << std::endl;
+}
+
+int main()
+{
+	int a[] = {6,8,6,9,5,2,1,6,4,6,3,9};
+	int b = 0;
+	b = sizeof(a)/sizeof(int);
+	printArray<int>(a, b);
+	insert_sort(a, b);
+	printArray<int>(a, b);
+}
+
+
